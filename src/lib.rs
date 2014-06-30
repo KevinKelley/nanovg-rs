9fed76f5b01570a28bef19dd4e2a39074455b6a2
Kevin Kelley <kevin@kelleysoft.com>
2014-06-30 09:54:56 -0600

    require libc types

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
 #![crate_id = "nanovg-rs"]
 #![crate_type = "lib"]
-#![deny(non_camel_case_types)]
-#![deny(unnecessary_parens)]
-#![deny(non_uppercase_statics)]
-#![deny(unnecessary_qualification)]
-#![warn(missing_doc)] // FIXME: should be denied.
-#![deny(unused_result)]
-#![deny(unnecessary_typecast)]
-#![warn(visible_private_types)] // FIXME: should be denied.
+//#![deny(non_camel_case_types)]
+//#![deny(unnecessary_parens)]
+//#![deny(non_uppercase_statics)]
+//#![deny(unnecessary_qualification)]
+//#![warn(missing_doc)] // FIXME: should be denied.
+//#![deny(unused_result)]
+//#![deny(unnecessary_typecast)]
+//#![warn(visible_private_types)] // FIXME: should be denied.
+
 //#![feature(globs)]
 //#![feature(macro_rules)]
 //#![feature(managed_boxes)]
 //#![feature(unsafe_destructor)]
-#![doc(html_root_url = "http://github.com/KevinKelley/nanovg-rs")]
+#![doc(html_root_url = "https://github.com/KevinKelley/nanovg-rs")]
+
+extern crate libc;
+
+pub mod nanovg;
 
