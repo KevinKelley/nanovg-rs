cc1a38e9c96d6bf7b9f09aa234d92a4db88d52e7
Kevin Kelley <kevin@kelleysoft.com>
2014-09-01 15:58:58 -0600

    hopefully compile on MacOSX

--- shim/src/shim.c
+++ shim/src/shim.c
@@ -1,7 +1,16 @@
 #define NANOVG_GL3_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/gl3.h>
+#include <OpenGL/glu.h>
+#include <OpenGL/glext.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/gl.h>
+#endif
+
 #include <GLFW/glfw3.h>
 
 #include <nanovg.h>
