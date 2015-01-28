2fce6ac8eee8117a44344353ef78d081fc31ec84
Nick Sarten <gen.battle@gmail.com>
2015-01-28 18:17:02 +1300

    Updated the build.
    These fixes should get the build working (hopefully). Changed some
    nonexistent (I'm guessing legacy) environment variables to current ones,
    the the variables I picked may not match up completely with the intent
    of the build script.

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
+/*
+At this stage this build script is just a shim to get the old build script
+working. At some point they'll both need to be refactored into a single
+unified build.rs
+*/
+
 use std::io::Command;
 
 fn main() {
+    // Build nanovg
     Command::new("make").arg("-f").arg("nanovg.mk").status().unwrap();
 }
--- nanovg.mk
+++ nanovg.mk
@@ -1,24 +1,22 @@
 NANOVG_REPO = https://github.com/memononen/nanovg
-NANOVG_DIR = $(DEPS_DIR)/nanovg
-LIBNANOVG = $(DEPS_DIR)/libnanovg.a
+NANOVG_DIR = $(OUT_DIR)/nanovg
+LIBNANOVG = $(OUT_DIR)/libnanovg.a
 
-ORIG_SHIM_DIR = $(CURDIR)/shim
-SHIM_DIR = $(DEPS_DIR)/shim
-LIBNANOVG_SHIM = $(DEPS_DIR)/libnanovg_shim.a
+ORIG_SHIM_DIR = $(PWD)/shim
+SHIM_DIR = $(OUT_DIR)/shim
+LIBNANOVG_SHIM = $(OUT_DIR)/libnanovg_shim.a
 
 $(LIBNANOVG_SHIM): $(LIBNANOVG)
-	mkdir -p "$(DEPS_DIR)"
-	cd "$(DEPS_DIR)" && ([ -d "shim" ] || cp -r "$(ORIG_SHIM_DIR)" .)
-	cd "$(SHIM_DIR)" && premake4 "--nanovg-out=$(DEPS_DIR)" "--nanovg-root=$(NANOVG_DIR)" gmake
+	mkdir -p "$(OUT_DIR)"
+	cd "$(OUT_DIR)" && ([ -d "shim" ] || cp -r "$(ORIG_SHIM_DIR)" .)
+	cd "$(SHIM_DIR)" && premake4 "--nanovg-out=$(OUT_DIR)" "--nanovg-root=$(NANOVG_DIR)" gmake
 	cd "$(SHIM_DIR)/build" && CFLAGS='-fPIC' make config=release verbose=1
-	cp "$(SHIM_DIR)/build/libnanovg_shim.a" "$(DEPS_DIR)"
+	cp "$(SHIM_DIR)/build/libnanovg_shim.a" "$(OUT_DIR)"
 
 $(LIBNANOVG):
-	mkdir -p "$(DEPS_DIR)"
-	#cd "$(DEPS_DIR)" && ([[ -d "nanovg" ]] && (cd nanovg; git pull) || git clone https://github.com/KevinKelley/nanovg)
-	cd "$(DEPS_DIR)" && ([ -d "nanovg" ] && (cd nanovg; git pull) || git clone $(NANOVG_REPO))
+	mkdir -p "$(OUT_DIR)"
+	#cd "$(OUT_DIR)" && ([[ -d "nanovg" ]] && (cd nanovg; git pull) || git clone https://github.com/KevinKelley/nanovg)
+	cd "$(OUT_DIR)" && ([ -d "nanovg" ] && (cd nanovg; git pull) || git clone $(NANOVG_REPO))
 	cd "$(NANOVG_DIR)" && premake4 gmake
 	cd "$(NANOVG_DIR)/build" && CFLAGS='-fPIC' make config=release verbose=1 nanovg
-	cp "$(NANOVG_DIR)/build/libnanovg.a" "$(DEPS_DIR)"
-
-
+	cp "$(NANOVG_DIR)/build/libnanovg.a" "$(OUT_DIR)"
